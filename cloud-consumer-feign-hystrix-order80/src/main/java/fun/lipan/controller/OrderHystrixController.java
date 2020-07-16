65793ec9a40974d4e12b22ca3613b6741d8a09db
drunkShrimper <416944223@qq.com>
2020-07-16 19:20:33 +0800

    加入Hystrix服务降级与服务熔断

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/fun/lipan/controller/OrderHystrixController.java
@@ -0,0 +1,60 @@
+package fun.lipan.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import fun.lipan.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author zzyy
+ * @create 2020/3/6 23:20
+ **/
+@RestController
+@Slf4j
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
+public class OrderHystrixController {
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+
+    /**
+     * 正常访问
+     * http://localhost/consumer/payment/hystrix/ok/32
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+    /**
+     * 超时访问
+     * http://localhost/consumer/payment/hystrix/timeout/32
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        //int age = 10/0;
+        return paymentHystrixService.paymentInfo_TimeOut(id);
+    }
+
+    /**
+     * 全局fallback
+     *
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息,请稍后重试.o(╥﹏╥)o";
+    }
+}
